25cea2c4d3a110f81ae8726e262322529e6dd7d0
Jason Noble <jasonn@jason-nobles-mac-mini.local>
2008-09-19 18:08:19 -0400

    Finished Chapter 6 Iteration A2 (section 6.3)

--- week1_finish/db/schema.rb
+++ week1_finish/db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20080919213026) do
+ActiveRecord::Schema.define(:version => 20080919215707) do
 
   create_table "products", :force => true do |t|
     t.string   "title"
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(:version => 20080919213026) do
     t.string   "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.decimal  "price",       :precision => 8, :scale => 2, :default => 0.0
   end
 
 end
