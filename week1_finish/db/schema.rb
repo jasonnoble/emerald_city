fb1182dc26da187330322861badcfffb45b5c4e0
Jason Noble <jasonn@jason-nobles-mac-mini.local>
2008-09-19 17:55:25 -0400

    Finished Chapter 6 Iteration A1 (sections 6.1 and 6.2)

--- week1_finish/db/schema.rb
+++ week1_finish/db/schema.rb
@@ -9,6 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20080919213026) do
+
+  create_table "products", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "image_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
