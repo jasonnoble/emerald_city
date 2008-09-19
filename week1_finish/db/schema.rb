74572b45303b1ec22f68bdd3ca7c36fb7e88b32f
Jason Noble <jasonn@jason-nobles-mac-mini.local>
2008-09-19 18:44:08 -0400

    Finished Chapter 6 Iteration A4 (section 6.5)

--- week1_finish/db/schema.rb
+++ week1_finish/db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20080919215707) do
+ActiveRecord::Schema.define(:version => 20080919222928) do
 
   create_table "products", :force => true do |t|
     t.string   "title"
