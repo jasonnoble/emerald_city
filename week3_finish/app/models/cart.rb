543c34c52affd9a079160a392e561d77127524de
Jason Noble <perlwizard@gmail.com>
2008-10-09 23:52:20 -0400

    Further refined the shopping cart to include cart items instead of products.

--- week3_finish/app/models/cart.rb
+++ week3_finish/app/models/cart.rb
@@ -6,6 +6,11 @@ class Cart
   end
   
   def add_product(product)
-    @items << product
+    current_item = @items.find {|item| item.product == product}
+    if current_item
+      current_item.increment_quantity
+    else
+      @items << CartItem.new(product)
+    end
   end
 end
--- week3_finish/db/schema.rb
+++ week3_finish/db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20080919222928) do
+ActiveRecord::Schema.define(:version => 20081010032427) do
 
   create_table "products", :force => true do |t|
     t.string   "title"
@@ -19,5 +19,15 @@ ActiveRecord::Schema.define(:version => 20080919222928) do
     t.datetime "updated_at"
     t.decimal  "price",       :precision => 8, :scale => 2, :default => 0.0
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
 end
