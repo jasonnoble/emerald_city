ddb1ca1ae7dc380374f00f24e3e91e22098d6929
Jason Noble <perlwizard@gmail.com>
2008-10-09 23:34:19 -0400

    Add the cart to the session cookie.

--- week3_finish/app/controllers/store_controller.rb
+++ week3_finish/app/controllers/store_controller.rb
@@ -2,5 +2,14 @@ class StoreController < ApplicationController
   def index
     @products = Product.find_products_for_sale
   end
-
+  
+  private
+  def find_cart
+    unless session[:cart]
+      # There is no cart, create one
+      session[:cart] = Cart.new
+    end
+    # Return new or existing cart
+    session[:cart]
+  end
 end
