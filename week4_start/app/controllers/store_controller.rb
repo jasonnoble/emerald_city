729ac655f1192e8b0bf1f63610c5da70f214684b
Jason Noble <perlwizard@gmail.com>
2008-10-18 08:59:21 -0400

    Changed the cart to render with a partial.  Iteration D1, section 9.1

--- week4_start/app/controllers/store_controller.rb
+++ week4_start/app/controllers/store_controller.rb
@@ -1,6 +1,7 @@
 class StoreController < ApplicationController
   def index
     @products = Product.find_products_for_sale
+    @cart = find_cart
   end
   
   def add_to_cart
@@ -12,6 +13,7 @@ class StoreController < ApplicationController
     else
       @cart = find_cart
       @cart.add_product(product)
+      redirect_to_index
     end
   end
   
@@ -21,8 +23,8 @@ class StoreController < ApplicationController
   end
   
   private
-  def redirect_to_index(msg)
-    flash[:notice] = msg
+  def redirect_to_index(msg = nil)
+    flash[:notice] = msg if msg
     redirect_to :action => :index
   end
   
