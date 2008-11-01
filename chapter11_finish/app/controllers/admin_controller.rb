9f36a850e73d45a6dae38bd1e82d78749a54c15f
Jason Noble <perlwizard@gmail.com>
2008-11-01 00:31:08 -0400

    Iteration F2, Chapter 11

---
+++ chapter11_finish/app/controllers/admin_controller.rb
@@ -0,0 +1,20 @@
+class AdminController < ApplicationController
+  def login
+    if request.post?
+      user = User.authenticate(params[:name], params[:password])
+      if user
+        session[:user_id] = user.id
+        redirect_to(:action => "index")
+      else
+        flash.now[:notice] = "Invalid user/password combination"
+      end
+    end
+  end
+
+  def logout
+  end
+
+  def index
+  end
+
+end
