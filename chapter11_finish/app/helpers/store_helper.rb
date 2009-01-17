33e7a2d8bb7a9e22f1ad02cf1503b1ed5ea6a808
Jason Noble <jasonn@jason-nobles-macbook-pro.local>
2009-01-17 15:16:22 -0500

    Moved to sub folder

--- chapter11_finish/app/helpers/store_helper.rb
+++
@@ -1,8 +0,0 @@
-module StoreHelper
-  def hidden_div_if(condition, attributes = {}, &block)
-    if condition
-      attributes["style"] = "display: none"
-    end
-    content_tag("div", attributes, &block)
-  end
-end
--- week3_start/app/controllers/store_controller.rb
+++
@@ -1,6 +0,0 @@
-class StoreController < ApplicationController
-  def index
-    @products = Product.find_products_for_sale
-  end
-
-end
