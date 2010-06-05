b1e7a674b7b10427503bfc96291ea7648b9f36f3
Jon Swope <jaswope@gmail.com>
2010-06-05 04:12:08 -0400

    Setting up MySQL on the box

--- agile_web_dev/chapter15_finish/cookbooks/vagrant_main/recipes/default.rb
+++ agile_web_dev/chapter15_finish/cookbooks/vagrant_main/recipes/default.rb
@@ -1,5 +1,13 @@
+execute "update apt" do
+  command "apt-get update"
+  action :run
+end
 
 gem_package "rails" do
   version "2.1.1"
   action :install
 end
+
+include_recipe "mysql::client"
+include_recipe "mysql::server"
+
